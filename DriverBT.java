09573ec0ff55cad7cdcc23fcc4a576bd1310a7d3
BrandonLeho <89223038+BrandonLeho@users.noreply.github.com>
2021-10-27 15:48:17 -0700

    Add files via upload

--- DriverBT.java
+++ DriverBT.java
@@ -1,18 +1,22 @@
+import java.util.Scanner;
+
 public class DriverBT 
 {
+	static Scanner scan = new Scanner(System.in);
+	
 	public static void main(String[] args)
-	{
+	{	
 		System.out.println("1st Testing Example:");
 		BinaryTree<String> aTree = new BinaryTree<>();
 		createTree1(aTree);
 		
 		System.out.print("(binaryTree) post-order: ");
 		aTree.postorderTraverse();
-		System.out.print("\n");
+		System.out.println();
 		
 		System.out.print("(binaryNode) post-order: ");
 		aTree.postorderTraverse_callBinaryNodeMethod();
-		System.out.print("\n");
+		System.out.println();
 		
 		System.out.println();
 		
@@ -33,11 +37,11 @@ public class DriverBT
 		
 		System.out.print("(binaryTree) post-order: ");
 		aTree2.postorderTraverse();
-		System.out.print("\n");
+		System.out.println();
 		
 		System.out.print("(binaryNode) post-order: ");
 		aTree2.postorderTraverse_callBinaryNodeMethod();
-		System.out.print("\n");
+		System.out.println();
 		
 		System.out.println();
 		
@@ -48,8 +52,11 @@ public class DriverBT
 		
 		System.out.println("(BinaryTree) # nodes of tree is " + aTree2.getNumberOfNodes());
 		System.out.println("(BinaryNode) # nodes of tree is " + aTree2.getNumberOfNodes_callBinaryNodeMethod());
+		System.out.println();	
 		
-		System.out.println("Done.");
+		System.out.print("Thank you for using our program :)");
+		
+		scan.close();
 	}  // end main
 
 	public static void createTree1(BinaryTree<String> tree)
@@ -81,29 +88,110 @@ public class DriverBT
 	/**Task 4: Create a tree case as shown in assignment 3*/
 	public static void createTree2(BinaryTree<String> tree)
 	{ 
+		String[] leaves = new String[4];
+		String[] nodes = new String[3];
+		
+		System.out.println("\nGiven Tree:\n");
+		System.out.println("     root     ");
+		System.out.println("    /   \\  ");
+	    System.out.println("  leaf1   parent  ");
+	    System.out.println("          /   \\");
+	    System.out.println("      parent   parent ");
+	    System.out.println("       /        /   \\");
+	    System.out.println("    leaf2    leaf3   leaf4");
+	    System.out.println();
+		
+		System.out.print("What do you want the root to be: ");
+		String root = scan.nextLine();
+		root = root.toUpperCase();
+		
+		for(int i = 0; i < leaves.length; i++)
+		{
+			System.out.print("(From left to right) What is the value of leaf number " + (i + 1) + ": ");
+			String leaf = scan.nextLine();
+			leaf = leaf.toUpperCase();		
+			
+			if(i != 1)
+			{
+				for(int j = 0; j < leaves.length; j++)
+				{
+					while(leaf.equals(leaves[j]))
+					{
+						System.out.print(leaf + " already exists as a node, choose a different character: ");
+						leaf = scan.nextLine();
+						leaf = leaf.toUpperCase();
+					}
+				}
+			}
+			
+			leaves[i] = leaf;
+		}
+		System.out.print("what is the parent node for " + leaves[1] + ": ");
+		String node1 = scan.nextLine();
+		node1 = node1.toUpperCase();
+		
+		for(int i = 0; i < nodes.length; i++)
+		{
+			while(node1.equals(leaves[i]) || node1.equals(nodes[i]))
+			{
+				System.out.print(node1 + " already exists as a node, choose a different character: ");
+				node1 = scan.nextLine();
+				node1 = node1.toUpperCase();
+			}
+		}
+		nodes[0] = node1;
+		
+		System.out.print("what is the parent node for " + leaves[2] + " and " +  leaves[3] + ": ");
+		String node2 = scan.nextLine();
+		node2 = node2.toUpperCase();
+		
+		for(int i = 0; i < nodes.length; i++)
+		{
+			while(node2.equals(leaves[i]) || node2.equals(nodes[i]))
+			{
+				System.out.print(node2 + " already exists as a node, choose a different character: ");
+				node2 = scan.nextLine();
+				node2 = node2.toUpperCase();
+			}
+		}
+		nodes[1] = node2;
+		
+		System.out.print("what is the parent node for " + nodes[0] + " and " +  nodes[1] + ": ");
+		String node3 = scan.nextLine();
+		node3 = node3.toUpperCase();
+		
+		for(int i = 0; i < nodes.length; i++)
+		{
+			while(node2.equals(leaves[i]) || node3.equals(nodes[i]))
+			{
+				System.out.print(node3 + " already exists as a node, choose a different character: ");
+				node3 = scan.nextLine();
+				node3 = node3.toUpperCase();
+			}
+		}
+		nodes[2] = node3;
+		
 		// Leaves
-		BinaryTree<String> bTree = new BinaryTree<>("B");
-		BinaryTree<String> fTree = new BinaryTree<>("F");
-		BinaryTree<String> gTree = new BinaryTree<>("G");
-		BinaryTree<String> hTree = new BinaryTree<>("H");
+		BinaryTree<String> tree1 = new BinaryTree<>(leaves[0]);
+		BinaryTree<String> tree2 = new BinaryTree<>(leaves[1]);
+		BinaryTree<String> tree3 = new BinaryTree<>(leaves[2]);
+		BinaryTree<String> tree4 = new BinaryTree<>(leaves[3]);
 
 		// Subtrees:
-		BinaryTree<String> dTree = new BinaryTree<>("D", fTree, null);
-		BinaryTree<String> eTree = new BinaryTree<>("E", gTree, hTree);
-		BinaryTree<String> cTree = new BinaryTree<>("C", dTree, eTree);
+		BinaryTree<String> tree5 = new BinaryTree<>(nodes[0], tree2, null);
+		BinaryTree<String> tree6 = new BinaryTree<>(nodes[1], tree3, tree4);
+		BinaryTree<String> tree7 = new BinaryTree<>(nodes[2], tree5, tree6);
 
-		tree.setTree("A", bTree, cTree);
+		tree.setTree(root, tree1, tree7);
 				
 		System.out.println("\nGiven Tree:\n");
-		System.out.println("     A     ");
-		System.out.println("   /   \\  ");
-	    System.out.println("  B     C  ");
-	    System.out.println("       / \\");
-	    System.out.println("      D   E ");
-	    System.out.println("     /   / \\");
-	    System.out.println("    F   G   H");
+		System.out.println("    " + root + "     ");
+		System.out.println("   / \\  ");
+	    System.out.println("  " + leaves[0] + "   " + nodes[2] + "  ");
+	    System.out.println("     / \\");
+	    System.out.println("    " + nodes[0] + "   " + nodes[1] + " ");
+	    System.out.println("   /   / \\");
+	    System.out.println("  " + leaves[1] + "   " + leaves[2] + "   " + leaves[3] + "");
 	    System.out.println();
-
 	} // end createTree2
-
 }  // end DriverBT
